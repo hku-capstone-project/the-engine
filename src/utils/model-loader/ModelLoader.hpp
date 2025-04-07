35a9d5f2b04b29b1dd0054c285338f79035ff9d6
Karm0tr1ne <657493712@qq.com>
2025-04-08 02:19:23 +0800

    Update Model Function

--- src/utils/model-loader/ModelLoader.hpp
+++ src/utils/model-loader/ModelLoader.hpp
@@ -1,5 +1,29 @@
 #pragma once
 
-class ModelLoader {
+#include <assimp/scene.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
 
+#include "volk.h"
+
+class Logger;
+
+struct Vertex {
+  glm::vec3 pos = glm::vec3();
+  glm::vec2 texCoord = glm::vec2();
+  glm::vec3 normal = glm::vec3();
+  glm::vec4 tangent = glm::vec4();
+
+  static inline std::array<VkVertexInputAttributeDescription, 4> GetAttributeDescriptions();
+  static inline VkVertexInputBindingDescription GetBindingDescription();
+};
+
+struct ModelAttributes {
+  std::vector<Vertex> vertices;
+  std::vector<uint32_t> indices;
+};
+
+namespace ModelLoader {
+ModelAttributes loadModelFromPath(const std::string &filePath, Logger *logger);
 };
---
+++ src/utils/vulkan-wrapper/memory/Model.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <array>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "utils/model-loader/ModelLoader.hpp"
+#include "utils/vulkan-wrapper/memory/Buffer.hpp"
+
+class VulkanApplicationContext;
+
+class Model {
+public:
+  Model(VulkanApplicationContext *appContext, Logger *logger, const std::string& filePath);
+  ~Model();
+  // disable move and copy
+  Model(const Model &) = delete;
+  Model &operator=(const Model &) = delete;
+  Model(Model &&) = delete;
+  Model &operator=(Model &&) = delete;
+
+private:
+  VulkanApplicationContext *_appContext;
+
+  std::vector<Vertex> _vertices;
+  std::vector<uint32_t> _indices;
+  std::shared_ptr<Buffer> _vertexBuffer;
+  int _vertCnt;
+  std::shared_ptr<Buffer> _indexBuffer;
+  int _idxCnt;
+
+  Logger *_logger;
+};
