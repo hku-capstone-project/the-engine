dc3e00eca553a2c6e6e5dd7d9a96da518b58e30c
danny <ruitian0716@gmail.com>
2025-05-27 01:54:30 +0800

    fixed a minor problem

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ set BINARY_DIR=build/%BUILD_TYPE%/
 set PROJECT_EXECUTABLE_PATH=%BINARY_DIR%apps/
 
 @REM delete build/Game folder if it exists
-if exist build/Game rd /s /q build/Game
+if exist build\Game rd /s /q build\Game
 @REM publish the game project in cs
 pushd game
 dotnet publish -c Release -r win-x64 --self-contained false -o ../build/Game
