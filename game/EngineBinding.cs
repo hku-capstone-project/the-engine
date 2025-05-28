ce41530d3ade3cf733193688f268b054aa4759b1
sdfsdfsv <2450225788@qq.com>
2025-05-28 23:43:14 +0800

    Add component and entity deletion support in EngineBindings.cs
    - Implemented RemoveComponentDel and DestroyEntityDel delegates for removing components (Transform, Velocity, Player) and entities.
    - Added RemoveTransform, RemoveVelocity, RemovePlayer, and DestroyEntity static methods, initialized in Init().
    - Fulfills task 3.2 from game/README.md for dynamic game world management.

--- game/EngineBinding.cs
+++ game/EngineBinding.cs
@@ -5,15 +5,27 @@ namespace Game
 {
     internal static class EngineBindings
     {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate uint CreateEntityDel();
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void AddTransformDel(uint e, Transform t);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void AddVelocityDel(uint e, Velocity v);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void AddPlayerDel(uint e, Player p);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void RemoveComponentDel(uint entityId);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void DestroyEntityDel(uint entityId);
 
         public static CreateEntityDel CreateEntity = null!;
         public static AddTransformDel AddTransform = null!;
         public static AddVelocityDel AddVelocity = null!;
         public static AddPlayerDel AddPlayer = null!;
+        public static RemoveComponentDel RemoveTransform = null!;
+        public static RemoveComponentDel RemoveVelocity = null!;
+        public static RemoveComponentDel RemovePlayer = null!;
+        public static DestroyEntityDel DestroyEntity = null!;
 
         public static void Init(Func<string, IntPtr> getProc)
         {
@@ -25,6 +37,45 @@ namespace Game
                                 getProc("AddVelocity"));
             AddPlayer = Marshal.GetDelegateForFunctionPointer<AddPlayerDel>(
                                 getProc("AddPlayer"));
+            RemoveTransform = Marshal.GetDelegateForFunctionPointer<RemoveComponentDel>(
+                                getProc("HostRemoveComponentTransform"));
+            RemoveVelocity = Marshal.GetDelegateForFunctionPointer<RemoveComponentDel>(
+                                getProc("HostRemoveComponentVelocity"));
+            RemovePlayer = Marshal.GetDelegateForFunctionPointer<RemoveComponentDel>(
+                                getProc("HostRemoveComponentPlayer"));
+            DestroyEntity = Marshal.GetDelegateForFunctionPointer<DestroyEntityDel>(
+                                getProc("HostDestroyEntity"));
         }
     }
 }
+
+// using System;
+// using System.Runtime.InteropServices;
+
+// namespace Game
+// {
+//     internal static class EngineBindings
+//     {
+//         public delegate uint CreateEntityDel();
+//         public delegate void AddTransformDel(uint e, Transform t);
+//         public delegate void AddVelocityDel(uint e, Velocity v);
+//         public delegate void AddPlayerDel(uint e, Player p);
+
+//         public static CreateEntityDel CreateEntity = null!;
+//         public static AddTransformDel AddTransform = null!;
+//         public static AddVelocityDel AddVelocity = null!;
+//         public static AddPlayerDel AddPlayer = null!;
+
+//         public static void Init(Func<string, IntPtr> getProc)
+//         {
+//             CreateEntity = Marshal.GetDelegateForFunctionPointer<CreateEntityDel>(
+//                                 getProc("CreateEntity"));
+//             AddTransform = Marshal.GetDelegateForFunctionPointer<AddTransformDel>(
+//                                 getProc("AddTransform"));
+//             AddVelocity = Marshal.GetDelegateForFunctionPointer<AddVelocityDel>(
+//                                 getProc("AddVelocity"));
+//             AddPlayer = Marshal.GetDelegateForFunctionPointer<AddPlayerDel>(
+//                                 getProc("AddPlayer"));
+//         }
+//     }
+// }
