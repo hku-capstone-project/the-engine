fd71f0c664300fce93bcfc5b761c2d6b6f6d4a46
Karmotrine. <657493712@qq.com>
2025-06-02 12:29:35 +0800

    Fix Shader's Bug of PushConstant

--- src/utils/vulkan-wrapper/pipeline/GfxPipeline.cpp
+++ src/utils/vulkan-wrapper/pipeline/GfxPipeline.cpp
@@ -152,11 +152,17 @@ void GfxPipeline::build() {
         throw std::runtime_error("Descriptor set layout is not created!");
     }
 
+    VkPushConstantRange constant_range{};
+    constant_range.size = sizeof(uint32_t);
+    constant_range.offset = 0;
+    constant_range.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+
     VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
     pipelineLayoutInfo.sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
     pipelineLayoutInfo.setLayoutCount         = 1;
     pipelineLayoutInfo.pSetLayouts            = &_descriptorSetBundle->getDescriptorSetLayout();
-    pipelineLayoutInfo.pushConstantRangeCount = 0;
+    pipelineLayoutInfo.pushConstantRangeCount = 1;
+    pipelineLayoutInfo.pPushConstantRanges = &constant_range;
 
     if (vkCreatePipelineLayout(_appContext->getDevice(), &pipelineLayoutInfo, nullptr,
                                &_pipelineLayout) != VK_SUCCESS) {
